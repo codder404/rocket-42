85109864ce947413e2e13a5030cd74869cd6013d
Fernando dos Santos <nando4803@gmail.com>
2020-10-04 21:20:58 +0100

    This commit add sidebar

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import Navbar from './Navbar';
+import Sidebar from './Sidebar';
 
 const Layout = ({ children }) => {
   return (
     <>
       <Navbar />
+      <Sidebar />
       {children}
     </>
   )
