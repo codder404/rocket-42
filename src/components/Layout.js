5b1cd973c5f6a33d29311a563fa79176b48db3b6
Fernando dos Santos <nando4803@gmail.com>
2020-10-04 22:09:38 +0100

    This commit update layout

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,13 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Navbar from './Navbar';
 import Sidebar from './Sidebar';
 
 const Layout = ({ children }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
   return (
     <>
-      <Navbar />
-      <Sidebar />
+      <Navbar toggle={toggle} />
+      <Sidebar isOpen={isOpen} toggle={toggle} />
       {children}
     </>
   )
