dd566747b28a30e5a10522e3d623174be0287e74
Fernando dos Santos <nando4803@gmail.com>
2020-10-05 22:52:36 +0100

    This commit update navbar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,21 +1,46 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Bars } from '@styled-icons/fa-solid';
+import { animateScroll as scroll } from 'react-scroll';
 
 import { menuLinks } from '../../resources/menuLinks';
 import * as N from './styles';
 
 function navbar({ toggle }) {
+  const [scrollNav, setScrollNav] = useState(false);
+
+  const changeNav = () => {
+    if(window.scrollY >= 80) {
+      setScrollNav(true);
+    } else {
+      setScrollNav(false);
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener('scroll', changeNav);
+  }, []);
+
+  const toggleHome = () => {
+    scroll.scrollToTop();
+  }
+
   return (
-    <N.Navbar>
+    <N.Navbar scrollNav={scrollNav}>
       <N.Container>
-        <N.NavbarLogo to='/'>Rocket</N.NavbarLogo>
+        <N.NavbarLogo to='/' onClick={toggleHome}>Rocket</N.NavbarLogo>
         <N.MobileIcon onClick={toggle}>
           <Bars />
         </N.MobileIcon>
         <N.NavbarMenu>
           <N.NavbarItem>
             {menuLinks.map(({ title, url }) => (
-              <N.NavbarLinks to={url}>{title}</N.NavbarLinks>
+              <N.NavbarLinks 
+              smooth={true} 
+              duration={500}
+              spay={true}
+              exact='true'
+              offset={-80}
+              to={url}>{title}</N.NavbarLinks>
             ))}
           </N.NavbarItem>
         </N.NavbarMenu>
