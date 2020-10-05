f12bc23998ec548d810acdd5b2b3229513bddbd8
Fernando dos Santos <nando4803@gmail.com>
2020-10-05 09:32:28 +0100

    This commit update content

--- src/resources/content.js
+++ src/resources/content.js
@@ -4,7 +4,7 @@ export const homeObjeOne = {
   lightText: true,
   lightTextDesc: true,
   topLine: 'Seu projeto sob demanda',
-  heading: 'Saiba como nossos processos funcionam e as vantagens competitivas que oferecemos no seu negócio.',
+  headLine: 'Saiba como nossos processos funcionam e as vantagens competitivas que oferecemos no seu negócio.',
   subtitle: 'Nosso time de tecnologia é capaz de entregar com alta qualidade e rapidez porque utilizamos metodologia ágeis de Desenvolvimento em Software.',
   buttonLabel: 'Solicitar Orçamento',
   imgStart: false,
