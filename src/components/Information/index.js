fd4dbc31d71cdd6bfeb9ba99b936decbe2c1e88d
Fernando dos Santos <nando4803@gmail.com>
2020-10-05 09:38:34 +0100

    This commit update info

--- src/components/Information/index.js
+++ src/components/Information/index.js
@@ -3,24 +3,36 @@ import React from 'react';
 import { Button } from '../Button';
 import * as F from './styles';
 
-function InfoSection () {
+function InfoSection ({
+  lightBg, 
+  id, 
+  imgStart, 
+  topLine, 
+  lightText, 
+  headLine, 
+  darkText, 
+  buttonLabel, 
+  description, 
+  img, 
+  alt
+}) {
   return (
-    <F.Container>
+    <F.Container lightBg={lightBg} id={id}>
       <F.InfoWrapper>
-        <F.InfoRow>
+        <F.InfoRow imgStart={imgStart}>
           <F.Column1>
             <F.TextWrapper>
-              <F.TopLine>TopLine</F.TopLine>
-              <F.Heading>Heading</F.Heading>
-              <F.Subtitle>Subtitle</F.Subtitle>
+              <F.TopLine>{topLine}</F.TopLine>
+              <F.Heading lightText={lightText}>{headLine}</F.Heading>
+              <F.Subtitle darkText={darkText}>{description}</F.Subtitle>
               <F.InfoBtnWrap>
-                <Button to='home'>Button</Button>
+                <Button to='home' primary big dark fontBig>{buttonLabel}</Button>
               </F.InfoBtnWrap>
             </F.TextWrapper>
           </F.Column1>
           <F.Column2>
             <F.ImgWrap>
-              <F.Img />
+              <F.Img src={img} alt={alt} />
             </F.ImgWrap>
           </F.Column2>
         </F.InfoRow>
