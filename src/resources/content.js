95d38a4cf4606ff658077ed8f8b45bbb9ac0770e
Fernando dos Santos <nando4803@gmail.com>
2020-10-05 09:21:59 +0100

    This commit add content & img

---
+++ src/resources/content.js
@@ -0,0 +1,16 @@
+export const homeObjeOne = {
+  id: 'about',
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: 'Seu projeto sob demanda',
+  heading: 'Saiba como nossos processos funcionam e as vantagens competitivas que oferecemos no seu negócio.',
+  subtitle: 'Nosso time de tecnologia é capaz de entregar com alta qualidade e rapidez porque utilizamos metodologia ágeis de Desenvolvimento em Software.',
+  buttonLabel: 'Solicitar Orçamento',
+  imgStart: false,
+  img: require('../assets/images/about.svg'),
+  alt: 'Team',
+  dark: true,
+  primary: true,
+  darkText: false
+}
