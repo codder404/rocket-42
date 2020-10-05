822a03957d314a3ab5e56dfe6d8b1fa1d40581db
Fernando dos Santos <nando4803@gmail.com>
2020-10-05 10:14:02 +0100

    This commit update content

--- src/resources/content.js
+++ src/resources/content.js
@@ -14,3 +14,37 @@ export const homeObjOne = {
   primary: true,
   darkText: false
 }
+
+export const homeObjTwo = {
+  id: 'sobre',
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: 'Seu projeto sob demanda',
+  headLine: 'Saiba como nossos processos funcionam e as vantagens competitivas que oferecemos no seu negócio.',
+  description: 'Nosso time de tecnologia é capaz de entregar com alta qualidade e rapidez porque utilizamos metodologia ágeis de Desenvolvimento em Software.',
+  buttonLabel: 'Solicitar Orçamento',
+  imgStart: false,
+  img: require('../assets/images/code.svg'),
+  alt: 'Team',
+  dark: true,
+  primary: true,
+  darkText: false
+}
+
+export const homeObjThree = {
+  id: 'sobre',
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: 'Seu projeto sob demanda',
+  headLine: 'Saiba como nossos processos funcionam e as vantagens competitivas que oferecemos no seu negócio.',
+  description: 'Nosso time de tecnologia é capaz de entregar com alta qualidade e rapidez porque utilizamos metodologia ágeis de Desenvolvimento em Software.',
+  buttonLabel: 'Solicitar Orçamento',
+  imgStart: false,
+  img: require('../assets/images/time.svg'),
+  alt: 'Team',
+  dark: true,
+  primary: true,
+  darkText: false
+}
